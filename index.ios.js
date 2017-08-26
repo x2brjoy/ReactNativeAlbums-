5d464c5579e92f28debfdae8838f5a9dfdb19489
x2brjoy <x2brjoy@gmail.com>
2017-08-26 15:44:23 -0500

    header component complete

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,12 @@
 // import library
 import React from 'react';
 import {Text, AppRegistry} from 'react-native';
+import Header from './src/components/header';
 
 // create a component
-const App = () => {
-  return (
-    <Text>Some text</Text>
-  );
-};
+const App = () => (
+	<Header headerText={'Albums'}></Header>
+);
 
 // render it to device
 AppRegistry.registerComponent('albums', () => App);
