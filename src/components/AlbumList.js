467c3c37dc9b006229eefcfe4fda573cd285ec9a
x2brjoy <x2brjoy@gmail.com>
2017-08-26 17:26:56 -0500

    card introduction

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import AlbumDetail from './AlbumDetail';
+import axios from 'axios';
 
 
 // class component (for handling dynamic content suck as ajax request)
 // lifecyle methods available 
 class AlbumList extends Component {
+	state = {albums: []};
+
+	// rules of state
+	// state: a plain javascript object used to record and respond to user-triggered events
+	// when we need to update a component shows, call 'this.setState', not 'this.state'
 	componentWillMount(){
-		console.log('component will mount in AlbumList');
+	axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+		.then(response => this.setState({albums: response.data}));
+		//console.log('component will mount in AlbumList');
 		//debugger;
 	}
 
+	renderAlbums (){
+		var returnVal = this.state.albums.map(album => 
+			<AlbumDetail key={album.title} album={album}/>
+		);
+
+		console.log(returnVal);
+		return returnVal;
+	}
+
 	render(){ // for class component, render
+		//console.log(this.state);
+
+
 		return (
 			<View>
-				<Text>Album List!!!</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}
